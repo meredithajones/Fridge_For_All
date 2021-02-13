e35f2fa8acb7742244cc7655cfdd863a9d10a2cf
Meredith Jones <mj19003@gmail.com>
2021-02-13 15:30:58 -0500

    Added ability for users to make inputs & console log

---
+++ client/src/components/Inventory/InventoryList.js
@@ -0,0 +1,28 @@
+import Inventory from './Inventory';
+import React, {useState} from 'react';
+
+function InventoryList() {
+    const[items, setItems] = useState([])
+
+    //Ensures that only text will pass through the input box
+    const addInventory = item => {
+        if(!item.text || /^\s*$/.test(item.text)) {
+            return
+        }
+
+        const newItem = [item, ...items]
+
+        setItems(newItem);
+        console.log(...items);
+    }
+
+
+    return (
+        <div>
+            <h2>Dropping Something Off? Picking something up? Please add or subtract it below:</h2>
+            <Inventory onSubmit={addInventory} />
+        </div>
+    );
+}
+
+export default InventoryList
--- server.js
+++ server.js
@@ -2,17 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const mongojs = require("mongojs");
 const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-  const databaseUrl = process.env.MONGODB_URI || "mongodb://localhost/inventory";
-  const collections = ["ppls_fridge"];
 
-  const db = mongojs(databaseUrl, collections);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fridge_for_all", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+const db = mongojs(databaseUrl, collections);
   
   db.on("error", error => {
     console.log("Database Error:", error);
@@ -24,7 +37,12 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 
 });
+module.exports = app;
