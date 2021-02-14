129edba3710a14258a767d233f95717ba938920e
Luke Martin <ljmart02@gmail.com>
2021-02-14 11:34:08 -0700

    UserRoutes to register and login users complete

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -3,52 +3,103 @@ const User = require("../models/userModel")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+
+//Register
 router.post("/", async (req, res) => {
-    try{
-    const {email, password, passwordVerify} = req.body
+    try {
+        const { email, password, passwordVerify } = req.body
 
-    //Validation
+        //Validation
 
-        if(!email || !password || !passwordVerify)
-        return res.status(400).json({errorMessage: "Please enter all required fields."})
+        if (!email || !password || !passwordVerify)
+            return res.status(400).json({ errorMessage: "Please enter all required fields." })
 
         if (password !== passwordVerify)
-        return res.status(400).json({errorMessage: "Please enter the same password twice"})
+            return res.status(400).json({ errorMessage: "Please enter the same password twice" })
 
-        const existingUser = await User.findOne({email: email});
+        const existingUser = await User.findOne({ email: email });
         if (existingUser)
-            return res.status(400).json ({
+            return res.status(400).json({
                 errorMessage: "Account with this email already exists"
             })
 
-    //Hashing password
+        //Hashing password
 
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
-    console.log(passwordHash)
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+        console.log(passwordHash)
 
-    //save a new user
-    const newUser = new User ({ 
-        email, passwordHash
-    })
-    const savedUser = await newUser.save();
+        //save a new user
+        const newUser = new User({
+            email, passwordHash
+        })
+        const savedUser = await newUser.save();
 
-    // sign the token
-    const token = jwt.sign({
-        user: savedUser._id
-    }, process.env.JWT_SECRET
-    );
+        // sign the token
+        const token = jwt.sign({
+            user: savedUser._id
+        }, process.env.JWT_SECRET
+        );
 
-    //send the token in a cookie
-    res.cookie("token", token, {
-        httpOnly: true,
-    });
+        //send the token in a cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+        })
+            .send();
 
     } catch (err) {
         console.error(err)
         res.status(500).send()
     }
 });
+
+//log in
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        //validate
+
+        if (!email || !password)
+            return res.status(400).json({ errorMessage: "Please enter all required fields." })
+
+        const existingUser = await user.findOne({ email });
+        if (!existingUser)
+            return res
+                .status(401).json({ errorMessage: "Wrong email or password" })
+
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash)
+        if (!passwordCorrect)
+            return res
+                .status(401).json({ errorMessage: "Wrong email or password" })
+
+        // sign the token
+        const token = jwt.sign({
+            user: existingUser._id
+        }, process.env.JWT_SECRET
+        );
+
+        //send the token in a cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+        })
+            .send();
+
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).send()
+    }
+});
+
+router.get("/logout", (req, res) => {
+    res.cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+        .send();
+})
 
 
 module.exports = router;
