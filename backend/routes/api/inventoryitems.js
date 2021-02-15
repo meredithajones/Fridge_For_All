b92cb1ccf63d319559020b7735457df4c9930011
Meredith Jones <mj19003@gmail.com>
2021-02-15 10:22:52 -0500

    API updates

--- backend/routes/api/inventoryitems.js
+++ backend/routes/api/inventoryitems.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const { default: inventory } = require('../../../client/src/components/Inventory/Inventory');
-let Inventory = require('../../models/Inventory.model');
 
+//Inventory Model
+const Inventory = require('../../models/Inventory.model');
+
+//route GET api/inventory
 router.route('/').get((req, res) => {
   Inventory.find()
     .then(inventory => res.json(inventory))
+    // .sort() can be used with quantity?
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
