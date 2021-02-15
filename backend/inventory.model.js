c6b11c733a1b262e40ce5c7908cd6bbbcacc2a46
Meredith Jones <mj19003@gmail.com>
2021-02-15 08:58:58 -0500

    Fixed typo in inventory.model.js

--- backend/inventory.model.js
+++ backend/inventory.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const inventorySchema = new Schema({
-  username: {
+    inventoryname: {
     type: String,
     required: true,
     unique: true,
