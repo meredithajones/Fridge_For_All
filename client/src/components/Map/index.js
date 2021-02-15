b941b330101634784e963e4a405d88f75fde1d1a
Luke Martin <ljmart02@gmail.com>
2021-02-15 11:44:51 -0700

    Mapquest Map displaying

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+const Mapquest=({ height, width, center, tileLayer, zoom, apiKey }) => {
+    useEffect(() => {
+        //api key
+        window.L.mapquest.key = apiKey
+
+        const map = window.L.mapquest.map("map", {
+            center,
+            layers: window.L.mapquest.tileLayer(tileLayer),
+            zoom
+    });
+    map.addControl(window.L.mapquest.control())
+}, []);
+
+return (
+    <div id="map" style={{ width, height }}>
+        <p> this is the  map</p>
+
+    </div>
+)
+}
+
+export default Mapquest;
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
 
