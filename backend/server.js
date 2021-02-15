d8edf05a065cec9cdd79534085f02d4146724005
Meredith Jones <mj19003@gmail.com>
2021-02-15 08:44:29 -0500

    Building outrouters

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+const inventoryRouter = require('./routes/inventoryitems');
+
+app.use('/inventoryitems', inventoryRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
