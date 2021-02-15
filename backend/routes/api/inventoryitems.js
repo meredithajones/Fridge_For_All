2284eb1929d4095a524687c67cd9c9a4e69440ad
Meredith Jones <mj19003@gmail.com>
2021-02-15 11:23:13 -0500

    Updates to models

--- backend/routes/api/inventoryitems.js
+++ backend/routes/api/inventoryitems.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { default: inventory } = require('../../../client/src/components/Inventory/Inventory');
 
 //Inventory Model
-const Inventory = require('../../models/Inventory.model');
+const Inventory = require('../../models/Inventorymodel');
 
 //route GET api/inventory
 router.route('/').get((req, res) => {
@@ -12,12 +12,14 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+//POST api/inventory
 router.route('/add').post((req, res) => {
   const inventoryname = req.body.inventoryname;
-  //username
-
+ 
   const newInventory = new Inventory({inventoryname});
 
+  //Save new inventory to the database
   newInventory.save()
     .then(() => res.json('Inventory added!'))
     .catch(err => res.status(400).json('Error: ' + err));
