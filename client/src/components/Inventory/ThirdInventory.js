97212a3a9a8eba38a4dbb880d2e29a489fc798f3
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-02-17 11:42:44 -0500

    fleshing out logic for color coding

--- client/src/components/Inventory/ThirdInventory.js
+++ client/src/components/Inventory/ThirdInventory.js
@@ -10,7 +10,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import uuid from "uuid";
 
-const App = ({keyword}) => {
+const App = ({ keyword }) => {
   //need item name, quantity
   const [items, setItems] = useState([
     { id: uuid(), itemName: "Kale", quantity: 1, isSelected: false },
@@ -21,7 +21,7 @@ const App = ({keyword}) => {
   const [inputValue, setInputValue] = useState("");
 
   //sorting variables
-  const [sortQuantity, setSortType] = useState('quantity');
+  const [sortQuantity, setSortType] = useState("quantity");
   const [data, setData] = useState([]);
 
   //When button is clicked a new item will be created with unique id from uuid,
@@ -35,57 +35,74 @@ const App = ({keyword}) => {
     };
 
     const newItems = [...items, newItem];
-
     setItems(newItems);
     setInputValue("");
   };
 
   const handleQuantityIncrease = (index) => {
     const newItems = [...items];
-
     newItems[index].quantity++;
-
     //when the arrows are pushed, the quantity sorts in real time
     setItems(newItems);
-	sortArray(sortQuantity);
+    sortArray(sortQuantity);
   };
 
   const handleQuantityDecrease = (index) => {
     const newItems = [...items];
-
     newItems[index].quantity--;
-
     //when the arrows are pushed, the quantity sorts in real time
     setItems(newItems);
-	sortArray(sortQuantity);
+    sortArray(sortQuantity);
   };
 
   const toggleDelete = (index) => {
     const newItems = [...items];
-
     newItems[index].isSelected = !newItems[index].isSelected;
-	
-	//clear the item from the array
-	newItems.splice(index, 1);
+    //clear the item from the array
+    newItems.splice(index, 1);
     setItems(newItems);
   };
 
   //sorting logic
-  const sortArray = quantity => {
-	const quantities = {
-	  quantities: 'quantity',
-	};
-	const sortProperty = quantities[quantity];
-	//sort by lowest quantity first
-	const sorted = [...items].sort((a, b) => a['quantity'] - b['quantity']);
-	console.log({sorted})
-	setItems(sorted);
+  const sortArray = (quantity) => {
+    const quantities = {
+      quantities: "quantity",
+    };
+    const sortProperty = quantities[quantity];
+    //sort by lowest quantity first
+    const sorted = [...items].sort((a, b) => a["quantity"] - b["quantity"]);
+    console.log({ sorted });
+    setItems(sorted);
   };
 
   //sorting function
   useEffect(() => {
     sortArray(sortQuantity);
-  }, []); 
+  }, []);
+
+  //when the qty drops below 5, it turns red
+  //quantities between 6-10 are gray
+  //quantities 11+ are blue
+  const qtyColor = (quantity: setItems) => {
+    if (quantity < 5) return urgent;
+    else if (quantity === 6) return good;
+    else if (quantity > 10) return surplus;
+  };
+
+  const urgent = {
+    color: "black",
+    backgroundColor: "red",
+  };
+
+  const good = {
+    color: "black",
+    backgroundColor: "lightgray",
+  };
+
+  const surplus = {
+    color: "black",
+    backgroundColor: "lightblue",
+  };
 
   return (
     <>
@@ -99,42 +116,44 @@ const App = ({keyword}) => {
         <FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
       </div>
       <div className="item-list">
-        {items.filter(item => item.itemName.toLowerCase().includes(keyword.toLowerCase())).map((item, index) => (
-          <div className="item-container">
-            <div className="item-name" onClick={() => toggleDelete(index)}>
-              {item.isSelected ? (
-                <>
-                  <FontAwesomeIcon icon={faCheckCircle} />
-                  <span className="deleted">{item.itemName}</span>
-                </>
-              ) : (
-                <>
-                  <FontAwesomeIcon icon={faCircle} />
-                  <span>{item.itemName}</span>
-                </>
-              )}
-            </div>
+        {items
+          .filter((item) =>
+            item.itemName.toLowerCase().includes(keyword.toLowerCase())
+          )
+          .map((item, index) => (
+            <div className="item-container">
+              <div className="item-name" onClick={() => toggleDelete(index)}>
+                {item.isSelected ? (
+                  <>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                    <span className="deleted">{item.itemName}</span>
+                  </>
+                ) : (
+                  <>
+                    <FontAwesomeIcon icon={faCircle} />
+                    <span>{item.itemName}</span>
+                  </>
+                )}
+              </div>
 
-            <div className="quantity">
-              <button>
-                <FontAwesomeIcon
-                  icon={faChevronLeft}
-                  onClick={() => handleQuantityDecrease(index)}
-                />
-              </button>
-			  {/* this is where the sorting needs to happen */}
-              <span>
-			    {item.quantity} 
-			  </span> 
-              <button>
-                <FontAwesomeIcon
-                  icon={faChevronRight}
-                  onClick={() => handleQuantityIncrease(index)}
-                />
-              </button>
+              <div className="quantity">
+                <button>
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    onClick={() => handleQuantityDecrease(index)}
+                  />
+                </button>
+                {/* this is where the sorting needs to happen */}
+                <span>{item.quantity}</span>
+                <button>
+                  <FontAwesomeIcon
+                    icon={faChevronRight}
+                    onClick={() => handleQuantityIncrease(index)}
+                  />
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
