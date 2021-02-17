3ec9a3d7ac5d4272ea3d0b5376118c2527b081f2
Meredith Jones <mj19003@gmail.com>
2021-02-17 10:28:30 -0500

    Added postitem to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
 export default {
+//Post Inventory Items
+postItem: function() {
+    return axios.post("/inventoryitems/" + id);
+},
+  
   //Gets All Inventory Items
   getItems: function() {
     return axios.get("/inventoryitems");
   },
   // Gets the item with the given id
   getItem: function(id) {
-    return axios.get("/inventoryitems" + id);
+    return axios.get("/inventoryitems/" + id);
   },
   // Deletes the item with the given id
   deleteItem: function(id) {
-    return axios.delete("/inventoryitems" + id);
+    return axios.delete("/inventoryitems/" + id);
   },
 };
