e2a779178fe56db20efd80094f0e804fa32c7db3
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-02-17 20:11:07 -0500

    fixing wild typos in locations and also adding more info

--- client/src/components/Aside/index.js
+++ client/src/components/Aside/index.js
@@ -5,43 +5,48 @@ import "./style.css";
 
 function Aside() {
   return (
-      <>
-    <div className="legend p-2 mb-2">
-            <h4 className="text-center">Get Started</h4>
-      Welcome to the community fridge. If you don't see your item listed, you
-      can add it in the field that says "Add something to the fridge...", and
-      hitting the plus sign. Alternatively, you can narrow the list by using the
-      search bar above. To delete an item, click on the circle next to the name
-      of the item.
-      <p />
-      After drop off, please increment accordingly so the fridge has the most
-      up-to-date quantities. If you take something from the fridge, please
-      subtract from the total. You can use the arrows on either side of the
-      quantity to add and subtract!
-    </div>
+    <>
+      <div className="legend p-2 mb-2">
+        <h4 className="text-center">Get Started</h4>
+        Welcome to the community fridge. If you don't see your item listed, you
+        can add it in the field that says "Add something to the fridge...", and
+        hitting the plus sign. Alternatively, you can narrow the list by using
+        the search bar above. To delete an item, click on the circle next to the
+        name of the item.
+        <p />
+        After drop off, please increment accordingly so the fridge has the most
+        up-to-date quantities. If you take something from the fridge, please
+        subtract from the total. You can use the arrows on either side of the
+        quantity to add and subtract!
+      </div>
       <div className="legend p-2">
-            <h4 className="text-center">Fridge Key</h4>
-            <div className="row mb-2">
-            <div className="col-2">
-                <FontAwesomeIcon icon={faSquare} id="urgent" /> 
-            </div>
-            <div className="col">
-                 Most urgently needed items! Donate these first!
-            </div></div>
-            <div className="row mb-2">
-            <div className="col-2">
-                <FontAwesomeIcon icon={faSquare} id="good" />  
-                </div>
-            <div className="col">
-                These items are in stock, but you can still donate if you want to.
-                </div></div>
-                <div className="row">
-            <div className="col-2">
-                <FontAwesomeIcon icon={faSquare} id="surplus" />  
-                </div><div className="col">
-                There is more than enough of these items, no need to donate.
-      </div></div></div>
-      </>
+        <h4 className="text-center">Fridge Key</h4>
+        <div className="row mb-2">
+          <div className="col-2">
+            <FontAwesomeIcon icon={faSquare} id="urgent" />
+          </div>
+          <div className="col">
+            Most urgently needed items! Donate these first!
+          </div>
+        </div>
+        <div className="row mb-2">
+          <div className="col-2">
+            <FontAwesomeIcon icon={faSquare} id="good" />
+          </div>
+          <div className="col">
+            These items are in stock, but you can still donate if you want to.
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <FontAwesomeIcon icon={faSquare} id="surplus" />
+          </div>
+          <div className="col">
+            There is more than enough of these items, no need to donate.
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- client/src/components/pages/Locations.js
+++ client/src/components/pages/Locations.js
@@ -2,6 +2,8 @@ import React from "react";
 import Mapquest from "../Map";
 import Row from "../Row";
 import Col from "../Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 function Locations() {
   const lat = "39.9526";
@@ -9,32 +11,6 @@ function Locations() {
 
   return (
     <div className="container rounded w-75 mx-auto d-block mt-5 mb-5">
-      <Row>
-        <Col size="md-6 sm-12">
-          <h4 className="mt-4">South Philly</h4>
-          1339 S 6th Street Philadelphia, PA <br />
-          1901 S 9th Street Philadelphia, PA
-          <h4 className="mt-4">Germantown</h4>
-          30 West Armat Street Philadelphia, PA
-          <h4 className="mt-4">West Philadelphia</h4>
-          511 S 53nd Street Philadelphia PA <br />
-          313 N 39th Street Philadelphia PA
-          <h4 className="mt-4">Northern Liberties</h4>
-          635 W Girard Ave Philadelphia PA <br />
-          915 Spring Garden Street Philadelphia PA
-        </Col>
-        <Col size="md-6 sm-12">
-          <h4 className="mt-4">Kensington</h4>
-          3400 Coral Street Philadelphia PA
-          <h4 className="mt-4">Brewerytown</h4>
-          3831 W Girard Ave Philadelphia PA
-          <h4 className="mt-4">Fishtown</h4>
-          1355 E Palmer Street Philadelphia Palmer
-          <h4 className="mt-4">Center City</h4>
-          1180 Ludlow Street Philadelphia PA
-        </Col>
-      </Row>
-      <br />
       <Row>
         <Col size="md-12 sm-12">
           <Mapquest
@@ -45,6 +21,52 @@ function Locations() {
             zoom={12}
             apiKey="KgkkF0XGvA7cEfndYAaPIGPwusNVFISG"
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-6 sm-12">
+          <h4 className="mt-4">South Philly</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Philadelphia Community Fridge
+          <br />
+          1229 S 6th Street (Alliance Medical)
+          <p />
+          <FontAwesomeIcon icon={faBuilding} /> Philadelphia Community Fridge
+          <br />
+          1901 S 9th Street (Bok)
+          <h4 className="mt-4">Germantown</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Germantown Fridge
+          <br />
+          20 West Armat Street (Greene St Friends School)
+          <h4 className="mt-4">West Philadelphia</h4>
+          <FontAwesomeIcon icon={faBuilding} /> The People's Fridge
+          <br />
+          511 S 52nd (Mina's World)
+          <p />
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br />
+          312 N 39th Street
+          <h4 className="mt-4">Northern Liberties</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br />
+          635 W Girard Ave (Ambassador)
+          <p />
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br />
+          915 Spring Garden Street (Triple Bottom Brewing)
+        </Col>
+        <Col size="md-6 sm-12">
+          <h4 className="mt-4">Kensington</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br /> 2400 Coral Street (Franny Lou's Porch)
+          <h4 className="mt-4">Brewerytown</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br /> 2821 W Girard Ave (Spot Burger)
+          <h4 className="mt-4">Fishtown</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br /> 1255 E Palmer Street (Castellino’s)
+          <h4 className="mt-4">Center City</h4>
+          <FontAwesomeIcon icon={faBuilding} /> Mamatee
+          <br /> 1180 Ludlow Street (Canopy Hotel)
         </Col>
       </Row>
     </div>
