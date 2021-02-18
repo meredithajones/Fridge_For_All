8fb2d7985b4521cb4deac92f4b325d1577e38deb
Meredith Jones <mj19003@gmail.com>
2021-02-17 19:25:56 -0500

    Updated API routes in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 export default {
-//Post Inventory Items
-saveItem: function(inventoryData) {
-    return axios.post("/inventoryitems/" + inventoryData);
-},
+
   //Gets All Inventory Items
   getItems: function() {
-    return axios.get("/inventoryitems");
+    return axios.get("api/inventoryitems");
   },
   // Gets the item with the given id
   getItem: function(id) {
-    return axios.get("/inventoryitems/" + id);
+    return axios.get("api/inventoryitems/" + id);
   },
   // Deletes the item with the given id
   deleteItem: function(id) {
-    return axios.delete("/inventoryitems/" + id);
+    return axios.delete("api/inventoryitems/" + id);
   },
+  //Post Inventory Items
+saveItem: function(inventoryData) {
+  return axios.post("api/inventoryitems/" + inventoryData);
+},
 };
